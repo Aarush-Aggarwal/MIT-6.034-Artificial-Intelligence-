2cdb8e9cef2bfb5129679686891848f466a5aca4
Aarush Aggarwal <46350653+Aarush-Aggarwal@users.noreply.github.com>
2021-08-31 20:54:10 +0530

    Update Lab_0.py

--- Lab_0.py
+++ Lab_0.py
@@ -21,6 +21,11 @@ print(count_pattern(('a', 'b', 'a'), ('g', 'a', 'b', 'a', 'b', 'a', 'b', 'a')))
 # Expression depth #
 --------------------
 
+# expression depth depends on the depths of all of its children (arguments). it is 1 more than the maximum of depths of its children. 
+# If you think of the expression as a tree, the expression depth is the height of the tree.
+# isinstance determines whether a variable points to a list. it takes two arguments: the variable to test, and the type (or tuple of types) to compare it to.
+# map(depth, expr) means apply the function "depth" to each element of expr.
+
 # For example:
 #  depth('x') => 0
 #  depth(('expt', 'x', 2)) => 1
@@ -28,8 +33,6 @@ print(count_pattern(('a', 'b', 'a'), ('g', 'a', 'b', 'a', 'b', 'a', 'b', 'a')))
 #  depth(('/', ('expt', 'x', 5), ('expt', ('-', ('expt', 'x', 2), 1), ('/', 5, 2)))) => 4
 
 --------------------------------------------------------------------------------------------
-# The depth of a list is one more than the maximum depth of its sub-lists.
-# map(depth, expr) means apply the function "depth" to each element of expr.
 
 def depth(expr):
     return max(map(depth, expr))+1 if isinstance(expr, tuple) else 0 
@@ -38,3 +41,22 @@ print(depth('x'))
 print(depth(('expt', 'x', 2)))
 print(depth(('+', ('expt', 'x', 2), ('expt', 'y', 2))))
 print(depth(('/', ('expt', 'x', 5), ('expt', ('-', ('expt', 'x', 2), 1), ('/', 5, 2)))))
+
+
+------------------
+# Tree reference #
+------------------
+
+# tree_ref procedure will take a tree and an index, and return the part of the tree (a leaf or a subtree) at that index.
+# for trees, indices will have to be lists of integers.
+
+--------------------------------------------------
+
+def tree_ref(tree, index):
+        return tree_ref(tree[index[0]], index[1:]) if index else tree
+
+tree = (((1, 2), 3), (4, (5, 6)), 7, (8, 9, 10))
+
+print(tree_ref(tree, (3, 1)))
+print(tree_ref(tree, (1, 1, 1)))
+print(tree_ref(tree, (0,)))
