7f760789e8ae5c0959ba3774b0de36e5e353819e
Aarush Aggarwal <46350653+Aarush-Aggarwal@users.noreply.github.com>
2022-01-10 23:08:55 +0530

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -5,6 +5,8 @@ import math
 log2 = lambda x: math.log(x, 2)
 INF = float('inf')
 
+# ID Trees
+
 ######################################################
 # Part 1A: Using an ID Tree to classify unknown points
 ######################################################
@@ -85,3 +87,74 @@ def find_best_classifier(data, possible_classifiers, target_classifier):
         raise api.NoGoodClassifiersError("the classifier has only one branch") 
     
     return best_classifier
+
+def construct_greedy_id_tree(data, possible_classifiers, target_classifier, id_tree_node=None):
+    if split_on_classifier(data, target_classifier).keys() == 1:
+        raise api.NoGoodClassifiersError("Target Classifier has only one branch")
+    
+    if id_tree_node is None:
+        id_tree_node = api.IdentificationTreeNode(target_classifier)
+        
+    best_classifier = find_best_classifier(data, possible_classifiers, target_classifier)
+    features_dict = split_on_classifier(data, best_classifier)
+    id_tree_node.set_classifier_and_expand(best_classifier, features_dict.keys())
+    
+    branches_dict = id_tree_node.get_branches()
+    
+    for branch in branches_dict:
+        branches_dict[branch] = construct_greedy_id_tree()
+    
+    return id_tree_node
+
+
+# k-Nearest Neighbors
+
+###########################
+# Part 2B: Distance metrics
+###########################
+
+def dot_product(u, v):
+    return sum(a*b for a, b in zip(u, v))
+
+def norm(v):
+    return math.sqrt(dot_product(v, v))
+
+def euclidean_distance(point1, point2):
+    return math.sqrt( sum([ (a-b)**2 for a, b in zip(point1.coords, point2.coords) ]) )
+
+def manhattan_distance(point1, point2):
+    return sum([ abs(a-b) for a, b in zip(point1.coords, point2.coords) ])
+
+def hamming_distance(point1, point2):
+    return sum([ a != b for a, b in zip(point1.coords, point2.coords) ])
+    
+def cosine_distance(point1, point2):
+    return 1 - ( dot_product(point1.coords, point2.coords) / norm(point1.coords) * norm(point2.coords) )
+
+
+#############################
+# Part 2C: Classifying Points
+#############################
+
+def get_k_closest_points(testPoint, data, k, distance_metric):
+    return list(dict(sorted( {point:distance_metric(testPoint, point) for point in data}.items(), key=lambda item: item[1] )))[:k]
+    
+def knn_classify_point(point, data, k, distance_metric):
+    from collections import Counter
+    
+    knn = get_k_closest_points(point, data, k, distance_metric)
+    classifications = [neighbor.classification for neighbor in knn]
+    return max(classifications, key=classifications.count)
+    
+    
+#######################################################################  
+# Part 2D: Choosing the best k and distance metric via cross-validation
+#######################################################################  
+
+def cross_validate(data, k, distance_metric):
+    for point in data:
+        data.remove(point)
+        cv = 0
+        if point.classification == knn_classify_point(point, data, k, distance_metric):
+            cv += 1
+        return float(cv/len(data))
